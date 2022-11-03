30a5b7fa9cdb388f7c27a28c2a7e48cbb75c8f89
Barry <barry.melton@gmail.com>
2022-11-03 16:40:09 -0400

    Fixes the indexer to accommodate diff baseURLs from requests

--- getSitemaps.py
+++ getSitemaps.py
@@ -3,27 +3,61 @@
 import requests
 import xml.etree.ElementTree as ET
 from urllib.parse import urlparse
-
+from shutil import rmtree
+import os
 
 base_url = "https://dwellsy-sitemap.us-west-2.elasticbeanstalk.com"
 output_dir = "./output"
 
 
 def save_file(url, folder, filename):
-    r = requests.get(url, allow_redirects=False, verify=False)
     out_filename = f"{output_dir}/{folder}/{filename}"
-    with open(out_filename, "wb") as f:
-        f.write(r.content)
+    r = requests.get(url, allow_redirects=False, verify=False)
+    if r.status_code != 200:
+        # try it again after parsing URL
+        url = f"{base_url}{urlparse(url).path}"
+        r = requests.get(url, allow_redirects=False, verify=False)
+        with open(out_filename, "wb") as f:
+            f.write(r.content)
+
+    else:
+        with open(out_filename, "wb") as f:
+            f.write(r.content)
+
+
+def save_index(objects, folder, filename):
+    print('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    out_filename = f"{output_dir}/{folder}/{filename}"
+    with open(out_filename, "a") as f:
+        f.write('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+        for item in objects:
+            loc = item["loc"]
+            lastmod = item["lastmod"]
+            f.write("  <sitemap>\n")
+            f.write(f"    <loc>{loc}</loc>\n")
+            f.write(f"    <lastmod>{lastmod}</lastmod>\n")
+            f.write("  <sitemap>\n")
+
+        f.write("</sitemapindex>\n")
+
+
+def cleanup():
+    try:
+        rmtree("./output")
+    except Exception:
+        pass
+
+    os.mkdir("./output/")
+    os.mkdir("./output/listing-history/")
 
 
 def main():
+    cleanup()
     r = requests.get(base_url, verify=False)
 
     root = ET.fromstring(r.content)
 
-    output_index = ""
-
-    xmlDict = {}
+    objects = []
 
     for sitemap in root:
         children = list(sitemap)
@@ -34,15 +68,14 @@ def main():
             pathparts = path.split("/")
             folder = pathparts[1]
             filename = pathparts[2]
-            print(f"""<sitemap>
-  <loc>{url}</loc>
-  <lastmod>{lastmod}</loc>
-</sitemap>""")
+            objects.append({"loc": url, "lastmod": lastmod})
             # print(url)
-            # save_file(x, folder, filename)
+            save_file(url, folder, filename)
 
-        # xmlDict[children[0].text] = children[1].text
-        # print (xmlDict[children[0]))
+    save_index(objects, "listing-history", "sitemapindex.xml")
+
+    # xmlDict[children[0].text] = children[1].text
+    # print (xmlDict[children[0]))
 
 
 if __name__ == "__main__":
